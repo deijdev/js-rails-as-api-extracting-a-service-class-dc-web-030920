b01a485604f733ec6cd8078546755902d8b58238
Deijah Wilson <deijahwilson1@gmail.com>
2020-04-26 18:45:21 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :sightings
+  resources :sightings, only: [:index, :show]
   resources :birds
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
